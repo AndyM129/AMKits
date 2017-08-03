5d4df968f634632ef81b4b48f5e900cb0614e94e
mengxinxin <mengxinxin@baidu.com>
2017-08-03 22:00:05 +0800

    优化项目结构

--- Example/Pods/Target Support Files/AMKits/AMKits-umbrella.h
+++ Example/Pods/Target Support Files/AMKits/AMKits-umbrella.h
@@ -16,11 +16,6 @@
 #import "UIView+AMKExtendTouchRect.h"
 #import "UIViewController+AMKLifeCircleBlock.h"
 #import "UIViewController+AMKViewControllerSwitch.h"
-#import "AMKEmojiHelper.h"
-#import "AMKEmojiManager.h"
-#import "NSDictionary+AMKEmojiHelper.h"
-#import "NSString+AMKEmojiHelper.h"
-#import "AMKLogManager.h"
 
 FOUNDATION_EXPORT double AMKitsVersionNumber;
 FOUNDATION_EXPORT const unsigned char AMKitsVersionString[];
